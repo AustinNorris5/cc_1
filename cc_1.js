c58db72bc3f1f5a547cefd14011160f738db3e93
AustinNorris5 <ajnorris@usf.edu>
2025-01-23 14:08:38 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,13 @@
+//Task 1: Variables
+let employeeName = "John Doe";
+const employeeID = 12345;
+var isActive = true;
+
+console.log(employeeName);
+console.log(typeof employeeName);
+
+console.log(employeeID);
+console.log(typeof employeeID);
+
+console.log(isActive);
+console.log(typeof isActive);
