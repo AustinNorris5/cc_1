5a986eb4f9d3204737cb30598b3eb24a75031d57
AustinNorris5 <ajnorris@usf.edu>
2025-01-24 12:42:18 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -11,3 +11,12 @@ console.log(typeof employeeID);
 
 console.log(isActive);
 console.log(typeof isActive);
+
+//Task 2: Primitive Data Types
+let productName = "Hat";
+const productPrice = 14.99;
+var isAvailable = true;
+
+console.log("productName:",productName,"Type:",typeof productName);
+console.log("productPrice:",productPrice,"Type:",typeof productPrice);
+console.log("isAvailable:",isAvailable,"Type:",typeof isAvailable);
